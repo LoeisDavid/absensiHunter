aabe953151d9464be1cd960ca9a69341e070ce94
LoeisDavid <materihunter1@gmail.com>
2026-05-13 23:45:37 +0700

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Laravel Tailwind</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+<body class="bg-gray-900 text-white flex items-center justify-center h-screen">
+
+    <h1 class="text-5xl font-bold text-blue-400">
+        Tailwind berhasil dipasang
+    </h1>
+
+</body>
+</html>
